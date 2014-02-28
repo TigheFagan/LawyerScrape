3599422e4374fb5deaf7dbb2827b7c53efd29608
TigheFagan <tfagan73@hotmail.com>
2014-02-28 16:28:42 -0700

    scrape search
    added search scrape; save to file in c:\temp; still rough

--- LawyerScape/LawyerScape/Program.cs
+++ LawyerScape/LawyerScape/Program.cs
@@ -14,60 +14,166 @@ namespace LawyerScape
         static void Main(string[] args)
         {
             HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
-
-            // There are various options, set as needed
             htmlDoc.OptionFixNestedTags = true;
-
-            // filePath is a path to a file containing the html
             WebClient client = new WebClient();
-            string downloadString = client.DownloadString("https://www.wyomingbar.org/directory/index.html?-Token.vid=3030");
-            htmlDoc.LoadHtml(downloadString);
 
-            // Use:  htmlDoc.LoadHtml(xmlString);  to load from a string (was htmlDoc.LoadXML(xmlString)
+            List<Lawyer> lawyers = new List<Lawyer>();
 
-            // ParseErrors is an ArrayList containing any errors from the Load statement
-            if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count() > 0)
+            bool isMore = true;
+            int linksPerPage = 0;
+            int loop = 20;
+            do
             {
-                foreach (var error in htmlDoc.ParseErrors)
-                {
-                    Console.WriteLine(error.Reason);
 
+                string detailUrl = "http://www.wyomingbar.org/directory/index.html?-Token.name=&-Token.firm=&-Token.city=&-Token.county=&-Token.sort=nameLast&-Token.lawyer_search_submitted=1&-Token.skip=" + (loop * linksPerPage);
+                string downloadString = client.DownloadString(detailUrl); ;
+                htmlDoc.LoadHtml(downloadString);
+                List<int> lawyerIDs = new List<int>();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    var searchLink = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table[2]/tr[" + (i + 2) + "]/td[1]/a");
+                    if (searchLink != null)
+                    {
+                        foreach (var a in searchLink.Attributes)
+                        {
+                            lawyerIDs.Add(int.Parse(a.Value.Substring(a.Value.IndexOf('=') + 1)));
+                        }
+                    }
+                    else
+                    {
+                        isMore = false;
+                        break;
+                    }
+                }
+
+
+                //filePath is a path to a file containing the html
+                foreach (int i in lawyerIDs)
+                {
+                    Lawyer lawyer = ProcessIndividual(i);
+                    lawyers.Add(lawyer);
+                }
+                loop++;
+            }
+            while (isMore);
+
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(@"C:\Temp\Lawyers.csv"))
+            {
+                foreach (Lawyer lawyer in lawyers)
+                {
+                    writer.WriteLine(lawyer.Name + "," +
+                        lawyer.Address.Replace(',', ' ').Replace('\n', ' ') + "," +
+                        lawyer.Website + "," +
+                        lawyer.Email + "," +
+                        lawyer.District + "," +
+                        lawyer.County + "," +
+                        lawyer.AdmissionDate + "," +
+                        lawyer.Status + "," +
+                        lawyer.Discipline);
                 }
             }
 
 
+            Console.ReadLine();
+        }
+
+        private static Lawyer ProcessIndividual(int i)
+        {
+            Lawyer lawyer = new Lawyer();
+
+            HtmlAgilityPack.HtmlDocument htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.OptionFixNestedTags = true;
+            WebClient client = new WebClient();
+            string detailUrl = "https://www.wyomingbar.org/directory/index.html?-Token.vid=" + i;
+            string downloadString = client.DownloadString(detailUrl); ;
+            htmlDoc.LoadHtml(downloadString);
+
             if (htmlDoc.DocumentNode != null)
             {
-                var addressAndPhoneNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/p[2]");
-                Console.WriteLine(addressAndPhoneNode.InnerHtml);
+                var test = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]");
+                if (!test.InnerHtml.Contains("Record is missing"))
+                {
+                    Console.WriteLine(i);
 
-                var nameNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/p[3]");
-                Console.WriteLine(nameNode.InnerHtml);
+                    try
+                    {
+                        var nameNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/p[2]");
+                        lawyer.Name = nameNode.InnerHtml;
 
-                var website = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr/td/a");
-                Console.WriteLine(website.InnerHtml);
+                        var addressAndPhoneNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/p[3]");
+                        lawyer.Address = addressAndPhoneNode.InnerHtml.Replace("<br>", " ");
+
+                        var website = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr/td/a");
+                        bool isMissingRow = false;
+                        if (website != null)
+                        {
+                            if (string.IsNullOrEmpty(website.InnerHtml))
+                            {
+                                isMissingRow = true;
+                            }
+                            else
+                            {
+                                var checkForNowWebsite = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[1]/td[3]");
+                                if (checkForNowWebsite != null && checkForNowWebsite.InnerHtml.Contains("Judicial District"))
+                                {
+                                    isMissingRow = true;
+                                }
+                                else
+                                {
+                                    lawyer.Website = website.InnerHtml;
+                                }
+                            }
+
+                            int firstRowIndex = isMissingRow ? 1 : 2;
+                            var email = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + firstRowIndex + "]/td/a");
+                            if (email != null)
+                            {
+                                isMissingRow = true;
+                                lawyer.Email = email.InnerHtml;
+                            }
 
 
-                var email = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[2]/td/a");
-                Console.WriteLine(email.InnerHtml);
+                            string districtLoc = "/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + firstRowIndex + "]/td[3]";
+                            var district = htmlDoc.DocumentNode.SelectSingleNode(districtLoc);
+                            lawyer.District = district.InnerHtml.Replace("Judicial District: ", "");
 
-                var district = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[2]/td[3]");
-                Console.WriteLine(district.InnerHtml);
+                            var county = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + (firstRowIndex + 1) + "]/td");
+                            lawyer.County = county.InnerHtml.Replace("County: ", "");
 
-                var county = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[3]/td");
-                Console.WriteLine(county.InnerHtml);
+                            var admissionDate = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + (firstRowIndex + 1) + "]/td[3]");
+                            lawyer.AdmissionDate = admissionDate.InnerHtml.Replace("Admission Date: ", "");
 
-                var admissionDate = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[3]/td[3]");
-                Console.WriteLine(admissionDate.InnerHtml);
+                            var status = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + (firstRowIndex + 2) + "]/td");
+                            lawyer.Status = status.InnerHtml.Replace("Status: ", "");
 
-                var status = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[4]/td");
-                Console.WriteLine(status.InnerHtml);
-
-                var discipline = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[5]/td");
-                Console.WriteLine(discipline.InnerHtml);
-
-                Console.ReadLine();
+                            var discipline = htmlDoc.DocumentNode.SelectSingleNode("/html/body/table[4]/tr/td[3]/table/tr[1]/td[2]/table/tr[" + (firstRowIndex + 3) + "]/td");
+                            lawyer.Discipline = discipline.InnerHtml.Replace("Public Discipline: ", "");
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("--------------------------------------------------\n");
+                        Console.WriteLine("Cannot Parse:" + i);
+                        Console.WriteLine("--------------------------------------------------\n");
+                    }
+                }
             }
-        }   
+            return lawyer;
+        }
+
+        private class Lawyer
+        {
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public string Website { get; set; }
+            public string Email { get; set; }
+            public string District { get; set; }
+            public string County { get; set; }
+            public string AdmissionDate { get; set; }
+            public string Status { get; set; }
+            public string Discipline { get; set; }
+
+        }
     }
 }
